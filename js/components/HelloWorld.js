4d37b02d7d7d2905edb0ef628a7fdce3fdc199ec
Charlie Hulcher <charlie.hulcher@gmail.com>
2017-02-10 15:27:12 -0500

    Organize a bit and lint

---
+++ js/components/HelloWorld.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+import styles from '../styles';
+
+const HelloWorld = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>
+        Welcome to React Native!
+      </Text>
+      <Text style={styles.instructions}>
+        To get started, edit index.*.js
+      </Text>
+    </View>
+  );
+};
+
+export default HelloWorld;
