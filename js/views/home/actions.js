f3e2bc4c80442fc06b3a0aaa4ec8708f5da5942f
Charlie Hulcher <charlie.hulcher@gmail.com>
2017-02-15 23:25:44 -0500

    FSA action creators

--- js/views/home/actions.js
+++ js/views/home/actions.js
@@ -3,6 +3,8 @@ import ActionTypes from '../../redux/action_types.json';
 export function getRandomNumber() {
   return {
     type: ActionTypes.GET_RANDOM,
-    random: Math.floor(Math.random() * 1e6),
+    payload: {
+      random: Math.floor(Math.random() * 1e6),
+    },
   };
 }
